9387a5bbc4af57a607a8f828fc02f50617d527bd
Adam Niedziałkowski <adam.niedzialkowski@gmail.com>
2017-04-10 14:04:15 +0200

    Adds Forgy k-means version

--- assignment 3/main_clustering.py
+++ assignment 3/main_clustering.py
@@ -15,18 +15,24 @@ def scale_center_points(points, scaling_factor=10):
 
 def plot_data_set(data_set):
     plt.figure()
+    plt.title('Generated 9 circular data clusters')
     plt.plot(data_set[:, 0], data_set[:, 1], 'r+')
     plt.show()
 
 
 def get_silhouette_scores(data, init, max_iter=20, num_runs=10):
+    print('Starting %s k-means...' % init)
     silhouette_scores = []
+    is_forgy = init is 'forgy'
     for num_iterations in range(1, max_iter+1):
         scores = []
         for run in range(num_runs):
-            kmeans_plusplus = KMeans(n_clusters=k, init='k-means++', max_iter=num_iterations).fit(data_set)
-            scores.append(silhouette_score(data_set, kmeans_plusplus.labels_))
+            if is_forgy :
+                init = data[np.random.choice(data.shape[0], k, replace=False)]
+            kmeans = KMeans(n_clusters=k, init=init, max_iter=num_iterations, n_init=1).fit(data_set)
+            scores.append(silhouette_score(data_set, kmeans.labels_))
         silhouette_scores.append(scores)
+    print('... finished.')
     return silhouette_scores
 
 radius = 5
@@ -35,6 +41,7 @@ generator = DatasetGenerator()
 k= 9
 num_runs = 30
 max_iterations = 20
+y_min, y_max = 0.4, 0.7
 
 centers = [Point((i - i % 3), (i % 3)) for i in range(3, 12)]# 3x3 grid
 centers = scale_center_points(centers, scaling_factor=scaling_factor)
@@ -46,19 +53,21 @@ for center in centers:
 
 data_set = np.concatenate(data_set)
 
-print('Check if data was generated correctly.')
+print('Check if data was generated correctly!')
 plot_data_set(data_set)
-print('Cool!')
-
 
 scores_kplusplus = get_silhouette_scores(data_set, init='k-means++')
 scores_random = get_silhouette_scores(data_set, init='random')
+scores_forgy = get_silhouette_scores(data_set, init='forgy')
 
 avg_plus = [np.average(scores_kplusplus[i]) for i in range(max_iterations)]
 std_plus = [np.std(scores_kplusplus[i]) for i in range(max_iterations)]
 
 avg_random = [np.average(scores_random[i]) for i in range(max_iterations)]
 std_random = [np.std(scores_random[i]) for i in range(max_iterations)]
+
+avg_forgy = [np.average(scores_forgy[i]) for i in range(max_iterations)]
+std_forgy = [np.std(scores_forgy[i]) for i in range(max_iterations)]
 
 iter = [(i+1) for i in range(max_iterations)]
 
@@ -67,16 +76,21 @@ fig.suptitle("Silhouette scores")
 
 sub = fig.add_subplot(221)
 sub.set_title('K-Means++')
-sub.errorbar(iter, avg_plus, yerr=std_plus, fmt='b+')
+sub.errorbar(iter, avg_plus, yerr=std_plus, fmt='bo')
+sub.set_ylim([y_min, y_max])
 
 sub = fig.add_subplot(222)
 sub.set_title('Random sampled')
-sub.errorbar(iter, avg_random, yerr=std_random, fmt='r+')
+sub.errorbar(iter, avg_random, yerr=std_random, fmt='bo')
+sub.set_ylim([y_min, y_max])
 
 sub = fig.add_subplot(223)
 sub.set_title('Forgy')
+sub.errorbar(iter, avg_forgy, yerr=std_forgy, fmt='bo')
+sub.set_ylim([y_min, y_max])
 
 sub = fig.add_subplot(224)
 sub.set_title('Random partitions')
+sub.set_ylim([y_min, y_max])
 
 plt.show()
